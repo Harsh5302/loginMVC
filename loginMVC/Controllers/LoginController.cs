bf81a39088614d8491bbac7bf4265f36a8d66f5e
harsh <harsh.patel27399@gmail.com>
2023-11-18 02:04:59 -0500

    Add project files.

---
+++ loginMVC/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using loginMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace loginMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        MvcUserDataEntities db = new MvcUserDataEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(MyLogin l)
+        {
+            var query = db.UserRegisters.SingleOrDefault(m => m.Email == l.Email && m.Password == l.Password);
+            if (query != null)
+            {
+                Response.Write("<script>alert('Login Successful')</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('Login unsuccessful.. Try again!!!')</script>");
+            }
+            return View();
+        }
+    }
+}
---
+++ loginMVC/Controllers/RegistrationController.cs
@@ -0,0 +1,32 @@
+﻿using loginMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace loginMVC.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        MvcUserDataEntities db = new MvcUserDataEntities(); 
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(UserRegister ur)
+        {
+            db.UserRegisters.Add(ur);
+            db.SaveChanges();
+            Response.Write("<script>alert('Registration Successful')</script>");
+            return View();
+        }
+    }
+    
+}
